c8f7ef15440728f5e874896d20619920ee0ed918
Mehmet Bayakı <mhmt_byk99@hotmail.com>
2023-06-15 01:41:30 +0300

    by Mevlut

--- OtelProje/Formlar/Tanımlamalar/FrmDurum.cs
+++ OtelProje/Formlar/Tanımlamalar/FrmDurum.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DevExpress.XtraEditors;
 using OtelProje.Entity;
 namespace OtelProje.Formlar.Tanımlamalar
 {
@@ -19,7 +21,27 @@ namespace OtelProje.Formlar.Tanımlamalar
         DbOtelEntities db = new DbOtelEntities();
         private void FrmDurum_Load(object sender, EventArgs e)
         {
-            gridControl1.DataSource = db.TblDurum.ToList();
+            db.TblDurum.Load();
+            bindingSource1.DataSource = db.TblDurum.Local;
+        }
+
+        private void gridView1_CellValueChanged(object sender, DevExpress.XtraGrid.Views.Base.CellValueChangedEventArgs e)
+        {
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                XtraMessageBox.Show("Lütfen değerleri kontrol edip yeniden giriş yapın!","Uyarı",MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void durumuSilToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bindingSource1.RemoveCurrent();
+            db.SaveChanges();
         }
     }
 }
