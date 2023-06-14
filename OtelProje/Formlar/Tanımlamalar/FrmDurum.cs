84be0d75fc6826b91b976c7ab7ad5c1ced0c5438
Mevlut Dızman <mdizman124@gmail.com>
2023-06-15 01:49:40 +0300

    Revert "by Mevlut"
    This reverts commit c8f7ef15440728f5e874896d20619920ee0ed918.

--- OtelProje/Formlar/Tanımlamalar/FrmDurum.cs
+++ OtelProje/Formlar/Tanımlamalar/FrmDurum.cs
@@ -2,13 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Data.Entity;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using DevExpress.XtraEditors;
 using OtelProje.Entity;
 namespace OtelProje.Formlar.Tanımlamalar
 {
@@ -21,27 +19,7 @@ namespace OtelProje.Formlar.Tanımlamalar
         DbOtelEntities db = new DbOtelEntities();
         private void FrmDurum_Load(object sender, EventArgs e)
         {
-            db.TblDurum.Load();
-            bindingSource1.DataSource = db.TblDurum.Local;
-        }
-
-        private void gridView1_CellValueChanged(object sender, DevExpress.XtraGrid.Views.Base.CellValueChangedEventArgs e)
-        {
-            try
-            {
-                db.SaveChanges();
-            }
-            catch (Exception)
-            {
-
-                XtraMessageBox.Show("Lütfen değerleri kontrol edip yeniden giriş yapın!","Uyarı",MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
-        }
-
-        private void durumuSilToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            bindingSource1.RemoveCurrent();
-            db.SaveChanges();
+            gridControl1.DataSource = db.TblDurum.ToList();
         }
     }
 }
